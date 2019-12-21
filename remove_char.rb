0316afc7277b9e7fa5cf35761455a803bfc88fb1
YuichiYagisawa <yuichi9915@gmail.com>
2019-12-21 15:15:19 +0900

    remove_char

---
+++ remove_char.rb
@@ -0,0 +1,16 @@
+def remove_char(s)
+  s[1..-2]
+end
+
+# ans
+
+# def remove_char(s)
+#   s.chop.reverse.chop.reverse
+# end
+
+# def remove_char(s)
+#   word = s.split('')
+#   word.pop
+#   word.shift  
+#   word.join
+# end
